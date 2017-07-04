65297bda2a3c53fdfe6594a9b705637bbd1a20e9
Konrad Słotwiński <kgslotwinski@gmail.com>
2017-07-04 09:43:45 +0200

    Update Component

---
+++ Component/Day.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FormioValidator\Component;
+
+use FormioValidator\Model\Component;
+
+class Day extends Component
+{
+    public function addCustomValidation()
+    {
+    }
+}
--- Component/PhoneNumber.php
+++ Component/PhoneNumber.php
@@ -4,7 +4,7 @@ namespace FormioValidator\Component;
 
 use FormioValidator\Model\Component;
 
-class Phonenumber extends Component
+class PhoneNumber extends Component
 {
     public function addCustomValidation()
     {
